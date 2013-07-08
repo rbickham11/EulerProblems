bca87cccd64efbc61e815823fa68cdb1fc5e6686
rbickham11 <rcbickha@svsu.edu>
2013-07-08 17:22:13 -0400

    Problems 29-31

--- EulerProblems/EulerMain.cs
+++ EulerProblems/EulerMain.cs
@@ -9,9 +9,10 @@ namespace EulerProblems
             var eu1 = new EulerProblems1_10();
             var eu11 = new EulerProblems11_20();
             var eu21 = new EulerProblems21_30();
+            var eu31 = new EulerProblems31_40();
             var eu61 = new EulerProblems61_70();
 
-            eu21.problem27();
+            eu31.problem31();
             Console.ReadKey();
         }
     }
---
+++ EulerProblems/EulerProblems31_40.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EulerProblems
+{
+    class EulerProblems31_40
+    {
+        private long limit;
+        private Stopwatch stopWatch = new Stopwatch();
+
+        public void runAll()
+        {
+            problem31();
+            //problem32();
+            //problem33();
+            //problem34();
+            //problem35();
+            //problem36();
+            //problem37();
+            //problem38();
+            //problem39();
+            //problem40();
+        }
+
+        public void problem31() //How many different ways can £2 be made using any number of coins?
+        {
+            stopWatch.Restart();
+            limit = 200;
+            int i, j;
+            int[] coins = { 1, 2, 5, 10, 20, 50, 100, 200 };
+            int[] ways = new int[limit + 1];
+            ways[0] = 1;
+
+            Console.Write("Problem 31: ");
+            for (i = 0; i < coins.Length; i++)
+                for (j = coins[i]; j <= limit; j++)
+                    ways[j] += ways[j - coins[i]];
+
+            Console.Write(ways[limit]);
+            stopWatch.Stop();
+            Console.WriteLine("  (" + stopWatch.ElapsedMilliseconds + "ms" + ")");
+        }
+    }
+}
